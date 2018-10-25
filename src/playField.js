d7b414d849cf572f907818e399c52af31e977049
eysin <eyloto@gmail.com>
2018-10-25 12:07:15 +0000

    The Wrapper classes have been created, work and are ready for further styling

---
+++ src/playField.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export class PlayField extends React.Component{
+    render(){
+        return <h1>This works</h1>
+    }
+}
+
+class TicTacToe extends React.Component{
+    render(){
+        return (<h1>TicTactoe works</h1>);
+    }
+}
