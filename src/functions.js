f519df8fb49074eb58410942e71c2f03067edc0b
unknown <rolandas17@ru.is>
2018-10-25 16:17:18 +0000

    commented and refined code of winCheck function

--- src/functions.js
+++ src/functions.js
@@ -1,30 +1,26 @@
+//checks if winner is found, if so, return player id (1 for x, 2 for o)
+//else returns 0
 function winCheck(grid)
 {
-    
-    for(var i = 0; i < 3;i++)
+    for(var j = 1; j < 3;j++)
     {
-        //horizontal check
-        if(grid[i][0] == 1 && grid[i][1] == 1 && grid[i][2] == 1)
-            return 1;
-        if(grid[i][0] == 2 && grid[i][1] == 2 && grid[i][2] == 2)
-            return 2;
-        //vertical check
-        if(grid[0][i] == 1 && grid[1][i] == 1 && grid[2][i] == 1)
-            return 1;
-        if(grid[0][i] == 2 && grid[1][i] == 2 && grid[2][i] == 2)
-            return 2;
-    }
+        for(var i = 0; i < 3;i++)
+        {
+            //horizontal check
+            if(grid[i][0] == j && grid[i][1] == j && grid[i][2] == j)
+                return j;
+            //vertical check
+            if(grid[0][i] == j && grid[1][i] == j && grid[2][i] == j)
+                return j;
+        }
     //diagonal check
-    if(grid[0][0] == 1 && grid[1][1] == 1 && grid[2][2] == 1)
-        return 1;
-    if(grid[0][0] == 2 && grid[1][1] == 2 && grid[2][2] == 2)
-        return 2;
+    if(grid[0][0] == j && grid[1][1] == j && grid[2][2] == j)
+        return j;
     //2nd diagonal check
-    if(grid[0][2] == 1 && grid[1][1] == 1 && grid[2][0] == 1)
-        return 1;
-    if(grid[0][2] == 2 && grid[1][1] == 2 && grid[2][0] == 2)
-        return 2;
+    if(grid[0][2] == j && grid[1][1] == j && grid[2][0] == j)
+        return j;
+    }
     //if none are true, ret 0
     return 0;
-} 
+}
 module.exports = winCheck;
