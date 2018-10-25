cbf615de6f66b3ababd10f5e6fe861b52ebb463c
unknown <rolandas17@ru.is>
2018-10-25 15:52:47 +0000

    implemented row check and tests

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,14 @@
 function winCheck(grid)
 {
-    if(grid[0][0] == 1 && grid[0][1] == 1 && grid[0][2] == 1)
-        return 1;
-    if(grid[0][0] == 2 && grid[0][1] == 2 && grid[0][2] == 2)
-        return 2;
+    
+    for(var i = 0; i < 3;i++)
+    {
+        //horizontal check
+        if(grid[i][0] == 1 && grid[i][1] == 1 && grid[i][2] == 1)
+            return 1;
+        if(grid[i][0] == 2 && grid[i][1] == 2 && grid[i][2] == 2)
+            return 2;
+    }
     return 0;
 } 
 module.exports = winCheck;
